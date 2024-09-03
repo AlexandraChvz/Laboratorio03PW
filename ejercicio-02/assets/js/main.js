a6a83592eb88e87a76e56910d23f6ed042df0a6b
AlexandraChvz <alexandra.hdzchavez2020@gmail.com>
2024-09-03 10:41:54 -0600

    new changes

--- ejercicio-02/assets/js/main.js
+++ ejercicio-02/assets/js/main.js
@@ -11,23 +11,42 @@ const multiply = (numero1, numero2) =>{
 
 } ;
 const split = (numero1, numero2) => {
+    return numero1/ numero2
 
 };
 
-const requestNumber = (mensaje) => {
-    let numero;
-    do {
-        numero = prompt(mensaje)
-    }
+
+
+const requestNumber = () =>  {
+    let numero1, numero2;
     
-};
+    
+    do {
+        numero1 = prompt("Introduce el primer número:");
+        if (isNaN(numero1) || numero1 === null || numero1.trim() === "") {
+            alert("Por favor, introduce un número válido.");
+        } else {
+            numero1 = parseFloat(numero1);
+        }
+    } while (isNaN(numero1));
 
-const selectOperation = () => {
-    let operacion;
-    const opciones = ['sumar', 'restar', 'multiplicar', 'dividir'];
-    do{
-        operacion = prompt("Elige una operacion ")
-    }while(!opciones)
+    do {
+        numero2 = prompt("Introduce el segundo número:");
+        if (isNaN(numero2) || numero2 === null || numero2.trim() === "") {
+            alert("Por favor, introduce un número válido.");
+        } else {
+            numero2 = parseFloat(numero2);
+        }
+    } while (isNaN(numero2));
+
+}
+
+
+const selectOperation = (operacion) => {
+    switch (operacion){
+        case 1 : plus()
+    }
+
 
 };
 
